106e66297e810087c34aa1f990b60d17d69f3749
Veasna <Veasna3d@gmail.com>
2024-03-11 14:12:46 +0700

    CRUD Product

--- resources/js/Pages/Admin/Components/Sidebar.vue
+++ resources/js/Pages/Admin/Components/Sidebar.vue
@@ -57,7 +57,7 @@ import { Link } from '@inertiajs/vue3';
                     </button>
                     <ul id="dropdown-pages" class="hidden py-2 space-y-2">
                         <li>
-                            <Link
+                            <Link :href="route('admin.products.index')"
                                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                             Product</Link>
                         </li>
